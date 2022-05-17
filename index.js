bb3792c471a66729d00f07b749e51bb5b3965dd2
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-05-17 21:54:27 +0600

    doctors added to server

--- index.js
+++ index.js
@@ -35,10 +35,11 @@ async function run() {
         const servicesCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('users');
+        const doctorCollection = client.db('doctors_portal').collection('doctors');
 
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = servicesCollection.find(query);
+            const cursor = servicesCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services);
         });
@@ -111,7 +112,7 @@ async function run() {
             else {
                 return res.status(403).send({ message: 'forbidden access' });
             }
-        })
+        });
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
@@ -124,6 +125,12 @@ async function run() {
             const result = await bookingCollection.insertOne(booking);
             return res.send({ success: true, result });
         });
+
+        app.post('/doctor', async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        })
 
     }
     finally {
